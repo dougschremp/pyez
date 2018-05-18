e4ce76cc6a57aadbe47e1453e165d29f217baec9
Douglas Schremp <dhs@acuity-link.com>
2018-05-17 23:14:47 -0400

    Updates

--- optical.py
+++ optical.py
@@ -10,41 +10,30 @@ import json
 import xmltodict
 
 
-hostname = input("Device hostname: ")
 username = input("Device username: ")
 password = getpass("Device password: ")
-
-dev = Device(host=hostname, user=username, passwd=password)
-try:
-    dev.open()
-except ConnectError as err:
-    print ("Cannot connect to device: {0}".format(err))
-    sys.exit(1)
-except Exception as err:
-    print (err)
-    sys.exit(1)
-
-#pprint (dev.facts)
-
-#pprint (dev.facts['re_info'])
-
-#XML Format default
-#data = dev.rpc.get_config('interfaces')
-#pprint(etree.tostring(data, encoding='unicode'))
-
-#fout=open("downtown-out.xml", "w")
-#pprint(etree.tostring(data, encoding='unicode'),stream=fout)
-
-#for interfaces in data.findall('.//interface'):
-#   pprint(etree.tostring(interfaces, encoding='unicode'))
-#   print (interfaces.get("<name>"))
-
-#print (type(data))
-
-
-optic=PhyPortDiagTable(dev)
-optic.get()
-pprint (json.loads(optic.to_json()),indent=4)
-
-
-dev.close()
+file= input ("Device list file:")
+with open(file) as fp:  
+    hostip = fp.readline().strip()
+    while hostip:
+        hostip.strip()
+        dev = Device(host=hostip, user=username, passwd=password)
+        try:
+            dev.open()
+        except ConnectError as err:
+            print ("Cannot connect to device: {0}".format(err))
+            sys.exit(1)
+            dev.close()
+            hostip = fp.readline().strip()
+            continue
+                 
+        hostname=dev.facts['hostname']
+        version=dev.facts['version']
+        serialnum=dev.facts['serialnumber']
+        print ( hostname," , ",version," , " , serialnum)
+        optic=PhyPortDiagTable(dev)
+        optic.get()
+        print (optic.rx_optic_power)
+        dev.close()
+        hostip = fp.readline().strip()
+        
